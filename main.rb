bba132b68967998d2f4cb0dc4deb378918242a5f
hanocha <saiki@feedforce.jp>
2016-12-07 00:27:45 +0900

    create class Jobcan

--- main.rb
+++ main.rb
@@ -2,26 +2,27 @@ require 'open-uri'
 require 'mechanize'
 require 'nokogiri'
 
-unless File.exist?('./code.conf')
-  print 'please input your code : '
-  code = gets.chomp
-  exit if code.empty?
-  File.open('./code.conf', 'w') do |f|
-    f.puts(code)
+class Jobcan
+  JOBCAN_BASE_URI = "https://ssl.jobcan.jp/employee/attendance?code="
+  attr_accessor :code, :agent, :page, :parsed_page
+
+  def initialize(code)
+    @code = code
+    @agent = Mechanize.new
+    @agent.user_agent = 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)'
   end
-else
-  code = File.open('./code.conf').gets.chomp
+
+  def get_page
+    top_page = @agent.get(JOBCAN_BASE_URI.join(@code))
+    @page = top_page.link_with(uri: top_page.links[4].uri).click
+  end
+
+  def parce_page
+    @parce_page = Nokogiri::HTML(main_page.body)
+  end
+
+  def work_days
+    @parce_page.xpath("//*[contains(./text(),'所定労働日数')]/following-sibling::*")
+  end
 end
 
-uri = "https://ssl.jobcan.jp/employee/attendance?code=#{code}"
-
-agent = Mechanize.new
-agent.user_agent = 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)'
-top_page = agent.get(uri)
-main_page = top_page.link_with(uri: top_page.links[4].uri).click
-
-doc = Nokogiri::HTML(main_page.body)
-work_days = doc.xpath("//body").css('div.infotpl > table.left > tbody > tr > td')[3].text.strip.slice(0,2)
-work_hours = 8 * work_days.to_i
-
-p work_hours
