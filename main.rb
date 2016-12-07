a40def6837ba155ac88d9c7d82cdfb53e0e63800
Hanocha <saiki@feedforce.jp>
2016-12-07 20:29:54 +0900

    必要そうな情報をスクレイピングするメソッドを作ってみた1

--- main.rb
+++ main.rb
@@ -13,16 +13,38 @@ class Jobcan
   end
 
   def get_page
-    top_page = @agent.get(JOBCAN_BASE_URI.join(@code))
+    top_page = @agent.get(JOBCAN_BASE_URI + @code)
     @page = top_page.link_with(uri: top_page.links[4].uri).click
   end
 
   def parce_page
-    @parce_page = Nokogiri::HTML(main_page.body)
+    @parce_page = Nokogiri::HTML(@page.body)
   end
 
-  def work_days
-    @parce_page.xpath("//*[contains(./text(),'所定労働日数')]/following-sibling::*")
+  def std_work_days
+    @parce_page.xpath("//*[contains(./text(),'所定労働日数')]/following-sibling::*").text.strip.slice(0,2).to_i
+  end
+
+  def std_work_hours
+    std_work_days * 8
+  end
+
+  def worked_days
+    @parce_page.xpath("//*[contains(./text(),'実働日数')]/following-sibling::*").text.strip.to_i
+  end
+
+  def worked_hours
+    @parce_page.xpath("//*[contains(./text(),'実労働時間')]/following-sibling::*").text.strip
+  end
+
+  def salaried_days
+    @parce_page.xpath("//*[contains(./text(),'有休(全休)')]/following-sibling::*").text.strip.to_i
+  end
+
+  def half_salaried_days
+    am = @parce_page.xpath("//*[contains(./text(),'有休(AM休)')]/following-sibling::*").text.strip.to_f
+    pm = @parce_page.xpath("//*[contains(./text(),'有休(PM休)')]/following-sibling::*").text.strip.to_f
+    am + pm
   end
 end
 
