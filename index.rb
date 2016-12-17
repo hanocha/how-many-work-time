dad9450ea8b72004bb645661f5631cc643f0c5e0
hanocha <saiki@feedforce.jp>
2016-12-17 17:29:59 +0900

    create error

--- index.rb
+++ index.rb
@@ -3,11 +3,14 @@ require 'sinatra/reloader'
 require './main.rb'
 
 get '/' do
-  @test = 'Hello, Sinatra!!!'
   slim :index
 end
 
 get '/jobcan' do
-  @j = Jobcan.new(@params[:code])
-  slim :jobcan
+  begin
+    @j = Jobcan.new(@params[:code])
+    slim :jobcan
+  rescue
+    slim :error
+  end
 end
