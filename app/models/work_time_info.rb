4ab2f8d02b553c19995dc8aade7948ff0c12f00f
hanocha <saiki@feedforce.jp>
2017-03-05 20:55:51 +0900

    勤怠情報を取得してその情報を持ったインスタンスを生成する

--- app/models/work_time_info.rb
+++ app/models/work_time_info.rb
@@ -1,5 +1,16 @@
 class WorkTimeInfo
-  def self.find
-    new
+  attr_reader :page
+
+  def self.find(code)
+    new(code)
+  end
+
+  def initialize(code)
+    @page = code
+    @agent = Mechanize.new
+    @agent.user_agent = 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)'
+    top = @agent.get(Rails.application.secrets.base_url + code)
+    main_page = top.link_with(uri: top.links[4].uri).click
+    @page = Nokogiri::HTML(main_page.body)
   end
 end
--- spec/models/work_time_info_spec.rb
+++ spec/models/work_time_info_spec.rb
@@ -2,8 +2,20 @@ require 'rails_helper'
 
 RSpec.describe WorkTimeInfo, type: :model do
   describe '.find' do
-    it '自身のインスタンスを返す' do
-      expect(WorkTimeInfo.find).to be_instance_of(WorkTimeInfo)
+    let(:code) { 'sample_of_code' }
+
+    context '勤怠情報の取得に成功した場合' do
+      it '自身のインスタンスを返す' do
+        expect(WorkTimeInfo.find(code)).to be_instance_of(WorkTimeInfo)
+      end
+
+      it '勤怠情報ページの情報を保存したインスタンスを返す' do
+        work_time_info = WorkTimeInfo.find(code)
+        expect(work_time_info.page).to be_instance_of(Nokogiri::HTML::Document)
+      end
+    end
+
+    context '勤怠情報の取得に失敗した場合' do
     end
   end
 end
