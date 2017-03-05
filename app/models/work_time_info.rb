4f82fe03e9844a6dc4e9026d3d0634c01c10cf9c
hanocha <saiki@feedforce.jp>
2017-03-05 21:42:55 +0900

    ページ取得リクエストのテストを書いた

--- app/models/work_time_info.rb
+++ app/models/work_time_info.rb
@@ -9,7 +9,7 @@ class WorkTimeInfo
     @page = code
     @agent = Mechanize.new
     @agent.user_agent = 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)'
-    top = @agent.get(Rails.application.secrets.base_url + code)
+    top = @agent.get("#{Rails.application.secrets.base_url}?code=#{code}")
     main_page = top.link_with(uri: top.links[4].uri).click
     @page = Nokogiri::HTML(main_page.body)
   end
--- spec/models/work_time_info_spec.rb
+++ spec/models/work_time_info_spec.rb
@@ -4,7 +4,43 @@ RSpec.describe WorkTimeInfo, type: :model do
   describe '.find' do
     let(:code) { 'sample_of_code' }
 
-    context '勤怠情報の取得に成功した場合' do
+    let!(:get_top_page_stub) do
+      stub_request(:get, "#{Rails.application.secrets.base_url}?code=#{code}").
+        with(query: { code: code }).
+        to_return(
+          status: 200,
+          body: %|<a href="#">a</a>
+<a href="#">b</a>
+<a href="#">c</a>
+<a href="#">d</a>
+<a href="/employee/attendance">e</a>
+          %|,
+          headers: { content_type: 'text/html' }
+        )
+    end
+
+    let!(:get_work_time_info_page_stub) do
+      stub_request(:get, "#{Rails.application.secrets.base_url}/attendance").
+        to_return(
+          status: 200,
+          body: 'test',
+          headers: { content_type: 'text/html' }
+        )
+    end
+
+    subject { WorkTimeInfo.find(code) }
+
+    context '勤怠情報の取得に成功したとき' do
+      it 'トップページを取得する' do
+        subject
+        expect(get_top_page_stub).to have_been_requested
+      end
+
+      it '出勤簿を取得する' do
+        subject
+        expect(get_work_time_info_page_stub).to have_been_requested
+      end
+
       it '自身のインスタンスを返す' do
         expect(WorkTimeInfo.find(code)).to be_instance_of(WorkTimeInfo)
       end
@@ -15,7 +51,7 @@ RSpec.describe WorkTimeInfo, type: :model do
       end
     end
 
-    context '勤怠情報の取得に失敗した場合' do
+    xcontext '勤怠情報の取得に失敗した場合' do
     end
   end
 end
