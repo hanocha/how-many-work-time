9cab16630a1fb9bc0a29592d24cc884beeb85d8d
hanocha <saiki@feedforce.jp>
2017-03-12 01:00:26 +0900

    work_time_info はサインイン済みの時しか見えない

--- app/controllers/work_time_infos_controller.rb
+++ app/controllers/work_time_infos_controller.rb
@@ -1,5 +1,13 @@
 class WorkTimeInfosController < ApplicationController
+  before_action :require_sign_in
+
   def show
-    @wti = WorkTimeInfo.find(params[:code])
+    @wti = WorkTimeInfo.find(current_user.code)
+  end
+
+  private
+
+  def require_sign_in
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
