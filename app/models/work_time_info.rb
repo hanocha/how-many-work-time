f33e84b846c8b31e4f9b2c79c99756de347630b6
hanocha <saiki@feedforce.jp>
2017-03-11 14:35:34 +0900

    WorkTimeInfo.find の実装を変更した
    コンストラクタを使わないようにした

--- app/models/work_time_info.rb
+++ app/models/work_time_info.rb
@@ -1,17 +1,13 @@
 class WorkTimeInfo
-  attr_reader :page
+  attr_accessor :page
 
   def self.find(code)
-    new(code)
-  end
-
-  def initialize(code)
-    @page = code
-    @agent = Mechanize.new
-    @agent.user_agent = 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)'
-    top = @agent.get("#{Rails.application.secrets.base_url}?code=#{code}")
+    wti = new
+    agent = Mechanize.new
+    agent.user_agent = 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)'
+    top = agent.get("#{Rails.application.secrets.base_url}?code=#{code}")
     main_page = top.link_with(uri: top.links[4].uri).click
-    @page = Nokogiri::HTML(main_page.body)
+    wti.tap { |i| i.page = Nokogiri::HTML(main_page.body) }
   end
 
   # 浮動小数点で表されている時間を時刻形式に変換して返す
