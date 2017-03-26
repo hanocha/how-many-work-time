2b1b80e0b11055891692ee9b65fcb31d450c6726
hanocha <saiki@feedforce.jp>
2017-03-26 17:24:02 +0900

    notifier#show のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resource :work_time_info, only: [:show]
   resource :work_time, only: [:show]
-  resource :notifier, only: [:create]
+  resource :notifier, only: [:show, :create]
 
   root 'work_times#show'
 end
--- spec/routing/notifiers_routing_spec.rb
+++ spec/routing/notifiers_routing_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe NotifiersController, type: :routing do
+  describe 'GET /notifier' do
+    it do
+      expect(get: '/notifier').
+        to route_to(controller: 'notifiers', action: 'show')
+    end
+  end
+
   describe 'POST /notifier' do
     it do
       expect(post: '/notifier').
