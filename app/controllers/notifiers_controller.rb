23952e446caec4306daede0bc1b7e4ba22cb94e4
hanocha <saiki@feedforce.jp>
2017-03-26 18:54:53 +0900

    Slack 通知機能の管理画面を作成

--- app/controllers/notifiers_controller.rb
+++ app/controllers/notifiers_controller.rb
@@ -1,8 +1,16 @@
 class NotifiersController < ApplicationController
   def show
+    @notifier = current_user.notifier || Notifier.new
   end
 
   def create
-    Notifier.create(user_id: current_user.id)
+    Notifier.create(notifier_params)
+    redirect_to notifier_path
+  end
+
+  private
+
+  def notifier_params
+    params.require(:notifier).permit(:user_id, :slack_user_name)
   end
 end
