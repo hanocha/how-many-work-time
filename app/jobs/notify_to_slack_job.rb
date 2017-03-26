902e90ba59d4327656b6a7c6f325c956ec4a46ba
hanocha <saiki@feedforce.jp>
2017-03-27 00:51:17 +0900

    Slack通知をONにしている人にIMを送れるJobを作成
    内容は仮のもの

--- app/jobs/notify_to_slack_job.rb
+++ app/jobs/notify_to_slack_job.rb
@@ -2,6 +2,12 @@ class NotifyToSlackJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+    notifiers = Notifier.all
+
+    client = Slack::Web::Client.new(token: Rails.application.secrets.slack_token)
+
+    notifiers.each do |n|
+      client.chat_postMessage(channel: "@#{n.slack_user_name}", text: "hello world from job!")
+    end
   end
 end
--- spec/jobs/notify_to_slack_job_spec.rb
+++ spec/jobs/notify_to_slack_job_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe NotifyToSlackJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#perform' do
+    let!(:slack_postMessage_stub) do
+      stub_request(:post, 'https://slack.com/api/chat.postMessage').
+        with(
+          body: hash_including(
+            channel: '@saiki',
+            token: Rails.application.secrets.slack_token,
+          )
+        ).and_return(
+          body: {
+            ok: true,
+            ts: '0',
+            channel: '12345678',
+            message: {},
+          }.to_json
+        )
+    end
+
+    let!(:notifier) { FactoryGirl.create(:notifier, slack_user_name: 'saiki') }
+
+    subject { NotifyToSlackJob.perform_now }
+
+    it 'Notifier を登録しているユーザに Slack の IM を送る' do
+      subject
+      expect(slack_postMessage_stub).to have_been_requested
+    end
+  end
 end
