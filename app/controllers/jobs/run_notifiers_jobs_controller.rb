2929a0324586e4871ca1d3040054a1b7443d1e67
hanocha <saiki@feedforce.jp>
2017-03-30 00:09:05 +0900

    Job実行用のエンドポイントにはベーシック認証をかける

--- app/controllers/jobs/run_notifiers_jobs_controller.rb
+++ app/controllers/jobs/run_notifiers_jobs_controller.rb
@@ -1,4 +1,11 @@
 class Jobs::RunNotifiersJobsController < ActionController::API
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+
+  http_basic_authenticate_with(
+    name: Rails.application.secrets.basic_auth_username,
+    password: Rails.application.secrets.basic_auth_password,
+  )
+
   def create
     NotifyToSlackJob.perform_later
     head :created
