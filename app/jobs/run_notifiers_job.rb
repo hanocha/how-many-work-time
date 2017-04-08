e92385b5eb09c5e99c524326e8c30faca8252547
hanocha <saiki@feedforce.jp>
2017-04-08 12:39:28 +0900

    $ rails g job run_notifiers

---
+++ app/jobs/run_notifiers_job.rb
@@ -0,0 +1,7 @@
+class RunNotifiersJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
---
+++ spec/jobs/run_notifiers_job_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe RunNotifiersJob, type: :job do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
