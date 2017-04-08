68b085323dff48e111192b5f59fb738752257dcd
hanocha <saiki@feedforce.jp>
2017-04-08 15:10:59 +0900

    NotifyToSlackJobは単一ユーザに通知するものに修正

--- app/jobs/notify_to_slack_job.rb
+++ app/jobs/notify_to_slack_job.rb
@@ -1,16 +1,15 @@
 class NotifyToSlackJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    notifiers = Notifier.all
+  def perform(notifier_id)
+    notifier = Notifier.find(notifier_id)
 
     client = Slack::Web::Client.new(token: Rails.application.secrets.slack_token)
 
-    notifiers.each do |n|
-      work_time_info = WorkTimeInfo.find(n.user.code) 
-      return if work_time_info.std_work_days == 0
+    work_time_info = WorkTimeInfo.find(notifier.user.code) 
+    return if work_time_info.std_work_days == 0
 
-      text = <<-EOS
+    text = <<-EOS
 :clock10: #{Time.now.strftime('%Y年%m月%d日')} の勤怠情報
 
 所定労働時間
@@ -29,7 +28,6 @@ class NotifyToSlackJob < ApplicationJob
 #{WorkTimeInfo.to_time(work_time_info.required_work_times)}
 EOS
 
-      client.chat_postMessage(channel: "@#{n.slack_user_name}", text: text)
-    end
+    client.chat_postMessage(channel: "@#{notifier.slack_user_name}", text: text)
   end
 end
